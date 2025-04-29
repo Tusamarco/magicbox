769ff317abeb836749837350021ebb6316f27bfd
tusa <tusamarco@gmail.com>
2025-04-29 19:48:23 +0200

    none

--- init.py
+++ init.py
@@ -2,8 +2,6 @@
 # -------
 import importlib
 import mysqlsh
-
-
 from mysqlsh.plugin_manager import plugin, plugin_function
 
 @plugin
@@ -22,44 +20,44 @@ print("Debugger attached.")
 
 
 
-@plugin_function("magicbox.helloWorld")
-def hello_world():
-    """
-    Simple function that prints "Hello world!"
+# @plugin_function("magicbox.helloWorld")
+# def hello_world():
+#     """
+#     Simple function that prints "Hello world!"
 
-    Just say Hello World
+#     Just say Hello World
 
-    Returns:
-        Nothing
-    """
-    print("Hello world!")
-    print("Just joking!")
+#     Returns:
+#         Nothing
+#     """
+#     print("Hello world!")
+#     print("Just joking!")
 
-@plugin_function("magicbox.showSchemas")
-def show_schemas(session=None):
-    """
-    Lists all database schemas
+# @plugin_function("magicbox.showSchemas")
+# def show_schemas(session=None):
+#     """
+#     Lists all database schemas
 
-    Sample function that works either with a session passed as parameter or
-    with the global session of the MySQL Shell.
+#     Sample function that works either with a session passed as parameter or
+#     with the global session of the MySQL Shell.
 
-    Args:
-        session (object): The optional session object used to query the
-            database. If omitted the MySQL Shell's current session will be used.
+#     Args:
+#         session (object): The optional session object used to query the
+#             database. If omitted the MySQL Shell's current session will be used.
 
-    Returns:
-        Nothing
-    """
-    if session is None:
-        shell = mysqlsh.globals.shell
-        session = shell.get_session()
-        if session is None:
-            print("No session specified. Either pass a session object to this "
-                "function or connect the shell to a database")
-            return
-    if session is not None:
-        r = session.run_sql("show schemas")
-        shell.dump_rows(r)
+#     Returns:
+#         Nothing
+#     """
+#     if session is None:
+#         shell = mysqlsh.globals.shell
+#         session = shell.get_session()
+#         if session is None:
+#             print("No session specified. Either pass a session object to this "
+#                 "function or connect the shell to a database")
+#             return
+#     if session is not None: 
+#         r = session.run_sql("show schemas")
+#         shell.dump_rows(r)
 
 from magicbox.proxysqlpkg import proxysql
 from magicbox.pxcpkg  import pxcprocessor 
